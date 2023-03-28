e21cfefd06edec17d0937cb48515d98b140c48e5
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-28 15:33:10 +0200

    Add class Person

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,6 @@
-public class Main {
-  /*Задача 1
-Создайте класс Person, которому при создании передавайте имя и дату рождения в виде строки в
-формате "ДД.ММ.ГГГГ".
+public class Person {
+    /*Задача 1
+
 Реализуйте в классе интерфейс Comparable<Person>, в котором "меньше" будет значить "младше".
 
 Подсказка: не обязательно вычислять возраст, этому мы так и не научились.
@@ -9,10 +8,24 @@ public class Main {
 Альтернативная подсказка: если очень хочется вычислять возраст, то можно это делать
 в миллисекундах с использованием getTime()!*/
 
-  public static void main(String[] args) {
-    Person person1 = new Person("Igor", "10.01.1989");
-    Person person2 = new Person("Oleg", "08.11.2002");
-    System.out.println(person1);
-    System.out.println(person2);
+  public final String name;
+  public final String dateOfBirth;
+
+  public Person(String name, String dateOfBirth) {
+    this.name = name;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  @Override
+  public String toString() {
+    return "Name: " + name + ", date of birthday: " + dateOfBirth;
+  }
+
+  public String getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public String getName() {
+    return name;
   }
 }
